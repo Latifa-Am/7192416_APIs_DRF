7158f47f4645edb5d68eec2cb2ee96d609a88fa4
Latifa Amahri <latifaamahri20@gmail.com>
2022-09-26 13:52:59 +0100

    adding Category/Product endpoints based on Viewset

--- shop/views.py
+++ shop/views.py
@@ -1,17 +1,16 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from shop.models import Category, Product
 from shop.serializers import CategorySerializer, ProductSerializer
 
-class CategoryAPIView(APIView):
-    def get(self, *args, **kwargs):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
+class CategoryViewset(ReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        return Category.objects.all()
         
-class ProductAPIView(APIView):
-    def get(self, *args, **kwargs):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+class ProductViewset(ReadOnlyModelViewSet):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
