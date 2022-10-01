66ef2b6781dd30ca90c764fc846a089b4afa3a07
Latifa Amahri <latifaamahri20@gmail.com>
2022-10-01 20:20:11 +0100

    Filtrez les résultats d'un endpoint

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,13 +1,19 @@
+from attr import field
 from rest_framework.serializers import ModelSerializer
 
-from shop.models import Category, Product
+from shop.models import Category, Product, Article
 
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ['id', 'date_created', 'date_updated', 'name']
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'date_created', 'date_updated', 'category']
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category']
+
+class ArticleSerializer(ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product']
--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,35 @@
+from unicodedata import category
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from sympy import Q, true
 
-from shop.models import Category, Product
-from shop.serializers import CategorySerializer, ProductSerializer
+from shop.models import Category, Product, Article
+from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
 
 class CategoryViewset(ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
 
     def get_queryset(self):
-        return Category.objects.all()
+        return Category.objects.filter(active=True)
         
 class ProductViewset(ReadOnlyModelViewSet):
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        return Product.objects.all()
+        # Nous récupérons tous les produits dans une variable nommé queryset
+        queryset = Product.objects.filter(active=True)
+        #Vérifions la présence du paramètre 'category_id' dans l'url et si oui alors appliquons notre filtre
+        category_id = self.request.GET.get('category_id')
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
+
+class ArticleViewset(ReadOnlyModelViewSet):
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.filter(active=True)
+        product_id = self.request.GET.get('product_id')
+        if product_id is not None:
+            queryset = queryset.filter(product_id=product_id)
+        return queryset
+        
